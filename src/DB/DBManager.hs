57c59f956e42f0c4d9d22da56224c6b8a0ea638e
unknown <ilhamabdillah123@gmail.com>
2024-10-31 11:50:08 +0700

    fix: connection pool parameter tuning

--- src/DB/DBManager.hs
+++ src/DB/DBManager.hs
@@ -9,13 +9,12 @@ type DBConnectionString = ByteString
 
 initConnectionPool :: DBConnectionString -> IO (Pool Connection)
 initConnectionPool connStr =
-  newPool
-    ( defaultPoolConfig
-        (connectPostgreSQL connStr)
-        close
-        60 -- The amount of seconds for which an unused resource is kept around
-        10 -- The maximum number of resources to keep open across all stripes
-    )
+  createPool
+    (connectPostgreSQL connStr)
+    close
+    1 -- Number of sub-pools (stripes)
+    60 -- The amount of seconds for which an unused resource is kept around
+    10 -- The maximum number of resources to keep open across all stripes
 
 connectionString :: DBConnectionString
 connectionString = pack "host=localhost port=5432 user=kolaboro password=kolaboroadmin dbname=kolaboro"
